4b61639ab0b712dc689cfab423066493cb2f8bbf
Gyongyver Biborka <biborka@budai-rfg.sulinet.hu>
2020-05-09 17:41:43 +0200

    most már nézi, hogy előtte Z van-e

--- after_z/after_atlag.py
+++ after_z/after_atlag.py
@@ -1,15 +1,23 @@
 text=""
 f=open("after_z.txt","r")
-text=f.read()
+for sor in f:
+    text+=sor.strip()
+    
+
 f.close()
 #print(text)
 db=0
 atlag=0
 szamok=['0','1','2','3','4','5','6','7','8','9']
-for karakter in text:
-    if karakter in szamok:
+elozo_karakter=text[0] #ez lesz mindig az, ami, ha 'z', akkor utána megvizsgáljuk, hogy szám van_e
+for i in range( len(text)-1): #utolsó előtti karakterig megyünk
+    
+    if elozo_karakter=='Z' and (text[i] in szamok):
+        print(elozo_karakter,' ', text[i])
         db+=1
-        atlag+=int(karakter)
-#print(atlag)
-#print(atlag/db)
+        atlag+=int(text[i])
+    elozo_karakter=text[i] #átírjuk az ekozo karaktert
+    
+print(atlag)
+print(atlag/db)
 print("keresett szám:",round(atlag/db,0))
